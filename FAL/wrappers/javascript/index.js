117bdbe17eebf484be5f57946bed5a829c8548d6
Caian Ertl <hi@caian.org>
2021-12-28 00:52:37 -0300

    feat: javascript wrapper test

---
+++ FAL/wrappers/javascript/index.js
@@ -0,0 +1,29 @@
+const { resolve } = require('path')
+const { promisify } = require('util')
+
+const { Library } = require('ffi-napi')
+
+function init () {
+    const exportedFunctions = {
+        addAndMultiplies: ['int', ['int']],
+        listS3Buckets: ['void', []]
+    }
+
+    const soPath = resolve(__dirname, '..', '..', '..', 'build', 'lib', 'libFAL.dylib')
+    const lib = Library(soPath, exportedFunctions)
+
+    /* sync */
+    const addAndMultipliesSync = (value) => lib.addAndMultiplies(value)
+    const listS3BucketsSync = () => lib.listS3Buckets()
+
+    /* async */
+    const addAndMultipliesPromise = promisify(lib.addAndMultiplies.async)
+    const listS3BucketsPromise = promisify(lib.listS3Buckets.async)
+
+    const addAndMultiplies = async (value) => addAndMultipliesPromise(value)
+    const listS3Buckets = async () => listS3BucketsPromise()
+
+    return { addAndMultiplies, addAndMultipliesSync, listS3Buckets, listS3BucketsSync }
+}
+
+module.exports = { init }
---
+++ tests/javascript/index.js
@@ -0,0 +1,18 @@
+const fal = require('../../FAL/wrappers/javascript')
+const myLib = fal.init()
+
+async function asyncTest () {
+    console.log('async main; first you should see the number, then the bucket list\n')
+
+    myLib.listS3Buckets()
+    myLib.addAndMultiplies(3).then((result) => console.log(result))
+}
+
+function syncTest () {
+    console.log('sync main; first you should see the bucket list, then the number\n')
+
+    myLib.listS3BucketsSync()
+    console.log(myLib.addAndMultipliesSync(3))
+}
+
+syncTest()
