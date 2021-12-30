5f5e6061a06e995d7cad3fa30c6fe85af038c399
Caian Ertl <hi@caian.org>
2021-12-30 04:24:09 -0300

    feat: shared lib function export (again)

--- fal/shared/fal.go
+++ fal/shared/fal.go
@@ -1,10 +1,36 @@
 package main
 
-import "C"
+import (
+	"C"
+	"context"
+	"fmt"
 
-// export SumAndMultiplies
-func SumAndMultiplies(value int) int {
-    return (value + 1) * value
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+//export __addAndMultiplies
+func __addAndMultiplies(value int) int {
+	return (value + 1) * value
+}
+
+//export __listS3Buckets
+func __listS3Buckets() {
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx)
+
+	client := s3.NewFromConfig(cfg)
+	params := s3.ListBucketsInput{}
+
+	result, err := client.ListBuckets(ctx, &params)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Found %d buckets\n", len(result.Buckets))
+	for _, bucket := range result.Buckets {
+		fmt.Println(*bucket.Name)
+	}
 }
 
 func main() {}
