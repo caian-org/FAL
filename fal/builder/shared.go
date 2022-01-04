20ab9525b848fb8cae9f73a54a2d67d0230d1dbd
Caian Ertl <hi@caian.org>
2022-01-03 21:00:26 -0300

    feat: function to create the shared lib directory

---
+++ fal/builder/shared.go
@@ -0,0 +1,42 @@
+package builder
+
+import (
+	_ "embed"
+	"fal/util"
+)
+
+var (
+	//go:embed shared/fal.go
+	_libfal_main []byte
+
+	//go:embed shared/_go.mod
+	_libfal_mod []byte
+
+	//go:embed shared/_go.sum
+	_libfal_sum []byte
+
+	//go:embed shared/Makefile
+	_libfal_make []byte
+)
+
+func InitSharedLib(buildDir *util.Location) error {
+	wd := buildDir.InnerLevel("shared")
+
+	err := wd.CreateDir()
+	if err != nil {
+		return err
+	}
+
+	f := util.FileList{
+		"go.mod":   _libfal_mod,
+		"go.sum":   _libfal_sum,
+		"Makefile": _libfal_make,
+	}
+
+    _, err = wd.CreateManyFiles(f)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
