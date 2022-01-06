793935a9ba3b50941b81cc572a00d5e54019221a
Caian Ertl <hi@caian.org>
2022-01-06 03:34:31 -0300

    feat: task file that builds dotnet projects

--- demo/functions/tasks.py
+++ demo/functions/tasks.py
@@ -1,3 +1,4 @@
+from os import listdir
 from os.path import join
 from os.path import exists
 
@@ -29,17 +30,24 @@ class Command:
         if self.pwd is not None:
             return exists(join(self.pwd, f))
 
-        return True
+        return False
 
-    def run(self, cmd):
+    def _has_ext(self, e):
+        if self.pwd is not None:
+            for f in listdir(self.pwd):
+                if f.endswith(e):
+                    return True
+
+        return False
+
+    def run(self, cmd, **kwargs):
         global COUNTER
         COUNTER += 1
 
         _cmd = cmd
         preffix = f'~~~ {COUNTER}. '
 
-        print(BRIGHT)
-        print(preffix, end='')
+        print('\n' + BRIGHT + preffix, end='')
 
         if self.pwd is not None:
             print(f'Changing directory to "{self.pwd}"')
@@ -47,10 +55,11 @@ class Command:
 
             cmd = f'cd {self.pwd} && {cmd}'
 
-        print(f'Running: {_cmd}')
-        print(RESET_ALL)
+        print('Running:')
+        print(' ' * (len(preffix) + 2), end='')
+        print(cmd + RESET_ALL + '\n')
 
-        self.c.run(cmd)
+        self.c.run(cmd, **kwargs)
         return self
 
 
@@ -76,9 +85,22 @@ class GradleProject(Command):
 
 
 # ~~~~
-class MultiLangProject(NodeProject, GradleProject):
+class DotNetProject(Command):
+    def dotnet_build(self):
+        if self._has_ext('.fsproj'):
+            self.run('dotnet restore')
+            self.run('dotnet tool install -g Amazon.Lambda.Tools --framework netcoreapp3.1', warn=True)
+            self.run('dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package package.zip')
+
+        return self
+
+
+# ~~~~
+class MultiLangProject(NodeProject, GradleProject, DotNetProject):
     def build(self):
         self.gradle_build()
+        self.dotnet_build()
+
         return self
 
 
@@ -98,4 +120,4 @@ def deploy(c, path):
 
 @task
 def remove(c, path):
-    _base_task(c, path, 'remove')
+    MultiLangProject(c, pwd=path).serverless('remove')
