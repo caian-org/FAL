101199d74e8bbe7189aa661d432b9da780bc25ac
Caian Ertl <hi@caian.org>
2022-01-11 22:48:27 -0300

    feat: function that adds 1 to a given number

---
+++ demo/functions/calc/plus-one/src/main/java/com/plusone/Handler.java
@@ -0,0 +1,41 @@
+package com.plusone;
+
+import java.util.Collections;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Handler implements RequestHandler<Map<String, String>, Response>
+{
+    private Response sendResponse (String output)
+    {
+        return Response.builder()
+            .setSuccess(output != null)
+            .setOutput(output)
+            .build();
+    }
+
+    @Override
+    public Response handleRequest (Map<String, String> event, Context context)
+    {
+        System.out.println("Got event: " + event);
+        String falKey = "_fal";
+
+        try {
+            if (!event.containsKey(falKey)) {
+                throw new NullPointerException(String.format("Event is missing \"%s\" property", falKey));
+            }
+
+            Request fal = Mapper.build().readValue(event.get(falKey), Request.class);
+            int input = Integer.parseInt(fal.getInput());
+
+            return this.sendResponse(String.valueOf(input + 1));
+        } catch (Exception e) {
+            System.out.println("Could not perform operation: " + e.getMessage());
+        }
+
+        return this.sendResponse(null);
+    }
+}
---
+++ demo/functions/calc/plus-one/src/main/java/com/plusone/Request.java
@@ -0,0 +1,16 @@
+package com.plusone;
+
+class Request
+{
+    private String input;
+
+    public String getInput()
+    {
+        return this.input;
+    }
+
+    public void setInput(String input)
+    {
+        this.input = input;
+    }
+}
