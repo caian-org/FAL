7b02b0a4eeab8f8a0d0afadb1c63c0bde3ce8ca0
Caian Ertl <hi@caian.org>
2022-04-02 20:01:36 -0300

    feat: greatly simplify the message structure and let it crash on error

--- demo/functions/basic/plus-one/src/main/java/com/plusone/Handler.java
+++ demo/functions/basic/plus-one/src/main/java/com/plusone/Handler.java
@@ -8,33 +8,17 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Handler implements RequestHandler<Map<String, String>, Response>
 {
-    private Response sendResponse (String output)
-    {
-        return Response.builder()
-            .setSuccess(output != null)
-            .setOutput(output == null ? "" : output)
-            .build();
-    }
-
     @Override
     public Response handleRequest (Map<String, String> event, Context context)
     {
         System.out.println("Got event: " + event);
+
         String falKey = "_fal";
-
-        try {
-            if (!event.containsKey(falKey)) {
-                throw new NullPointerException(String.format("Event is missing \"%s\" property", falKey));
-            }
-
-            Request fal = Mapper.build().readValue(event.get(falKey), Request.class);
-            int input = Integer.parseInt(fal.getInput());
-
-            return this.sendResponse(String.valueOf(input + 1));
-        } catch (Exception e) {
-            System.out.println("Could not perform operation: " + e.getMessage());
+        if (!event.containsKey(falKey)) {
+            throw new NullPointerException(String.format("Event is missing \"%s\" property", falKey));
         }
 
-        return this.sendResponse(null);
+        int input = Integer.parseInt(event.get(falKey));
+        return new Response(String.valueOf(input + 1));
     }
 }
--- demo/functions/basic/plus-one/src/main/java/com/plusone/Response.java
+++ demo/functions/basic/plus-one/src/main/java/com/plusone/Response.java
@@ -1,46 +1,11 @@
 package com.plusone;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Response
 {
     public String _fal;
 
-    public Response(Object data)
+    public Response(String output)
     {
-        try {
-            this._fal = Mapper.build().writeValueAsString(data);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Could not process JSON: " + e.getMessage());
-        }
-    }
-
-    public static Builder builder()
-    {
-        return new Builder();
-    }
-
-    public static class Builder
-    {
-        private boolean success;
-        private String output;
-
-        public Builder setSuccess(boolean success)
-        {
-            this.success = success;
-            return this;
-        }
-
-        public Builder setOutput(String output)
-        {
-            this.output = output;
-            return this;
-        }
-
-        public Response build()
-        {
-            return new Response(this);
-        }
+        this._fal = output;
     }
 }
