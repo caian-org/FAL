d4ceb76700fba3e53091178ff3885f61aa53dea5
Caian Ertl <hi@caian.org>
2022-04-03 00:13:50 -0300

    chore: change framework for .NET projects to NET6

--- misc/proj_dotnet.py
+++ misc/proj_dotnet.py
@@ -4,7 +4,7 @@ from misc.command import Command
 class DotNetProject(Command):
     def dotnet_prepare(self):
         if self._has_ext('.fsproj') or self._has_ext('.csproj'):
-            fw = '--framework netcoreapp3.1'
+            fw = '--framework net6.0'
 
             self.run('dotnet restore')
             self.run(f'dotnet tool install -g Amazon.Lambda.Tools {fw}', warn=True)
