6bfefc63f3ef05b953b2ecb93de56905a3388ecf
Caian Ertl <hi@caian.org>
2022-04-09 19:12:35 -0300

    style: format

--- fal/manifest/tojson.go
+++ fal/manifest/tojson.go
@@ -3,52 +3,52 @@
 package manifest
 
 import (
-    "os"
-    "path/filepath"
-    "strings"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"fal/shared/fs"
 	"sigs.k8s.io/yaml"
 )
 
 func convertYamlFileToJson(path string, info os.FileInfo, err error) error {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    if info.IsDir() || !strings.HasSuffix(path, ".yml") {
-        return nil
-    }
+	if info.IsDir() || !strings.HasSuffix(path, ".yml") {
+		return nil
+	}
 
-    schemaLevel := fs.NewLocation(filepath.Dir(path))
-    schemaFilename := info.Name()
+	schemaLevel := fs.NewLocation(filepath.Dir(path))
+	schemaFilename := info.Name()
 
-    yamlSchema, err := schemaLevel.ReadFile(schemaFilename)
-    if err != nil {
-        panic(err)
-    }
+	yamlSchema, err := schemaLevel.ReadFile(schemaFilename)
+	if err != nil {
+		panic(err)
+	}
 
-    jsonSchema, err := yaml.YAMLToJSON(yamlSchema)
-    if err != nil {
-        panic(err)
-    }
+	jsonSchema, err := yaml.YAMLToJSON(yamlSchema)
+	if err != nil {
+		panic(err)
+	}
 
-    safeFL := []rune(schemaFilename)
-    jsonSchemaFilename := string(safeFL[:len(safeFL) - 4]) + ".json"
+	safeFL := []rune(schemaFilename)
+	jsonSchemaFilename := string(safeFL[:len(safeFL)-4]) + ".json"
 
-    _, err = schemaLevel.CreateFile(jsonSchemaFilename, jsonSchema)
-    if err != nil {
-        panic(err)
-    }
+	_, err = schemaLevel.CreateFile(jsonSchemaFilename, jsonSchema)
+	if err != nil {
+		panic(err)
+	}
 
-    return nil
+	return nil
 }
 
 func CreateJsonSchemaFiles() {
-    pwd, err := os.Getwd()
-    if err != nil {
-        panic(err)
-    }
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 
-    filepath.Walk(filepath.Join(pwd, "manifest", "manifest.json"), convertYamlFileToJson)
+	filepath.Walk(filepath.Join(pwd, "manifest", "manifest.json"), convertYamlFileToJson)
 }
