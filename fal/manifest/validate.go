4e76a7ebfc4abc116067c5ba08700a6a1e9d7feb
Caian Ertl <hi@caian.org>
2022-04-09 20:17:42 -0300

    feat: schema validation for the rest of the manifest file

--- fal/manifest/validate.go
+++ fal/manifest/validate.go
@@ -18,6 +18,27 @@ var (
 
 	//go:embed manifest.json/meta/schema.json
 	_schema_doc_meta string
+
+	//go:embed manifest.json/target/schema.json
+	_schema_doc_target string
+
+	//go:embed manifest.json/function/schema.json
+	_schema_doc_func string
+
+	//go:embed manifest.json/function/cache-policy.json
+	_schema_doc_func_cache string
+
+	//go:embed manifest.json/assembly/schema.json
+	_schema_doc_assembly string
+
+	//go:embed manifest.json/assembly/step/call.json
+	_schema_doc_assembly_step_call string
+
+	//go:embed manifest.json/assembly/step/map.json
+	_schema_doc_assembly_step_map string
+
+	//go:embed manifest.json/assembly/step/series.json
+	_schema_doc_assembly_step_series string
 )
 
 func loadJSON(c string) gjs.JSONLoader {
@@ -26,7 +47,16 @@ func loadJSON(c string) gjs.JSONLoader {
 
 func validateSchema(manifest *FALManifest) error {
 	loader := gjs.NewSchemaLoader()
-	loader.AddSchemas(loadJSON(_schema_doc_meta))
+	loader.AddSchemas(
+        loadJSON(_schema_doc_meta),
+        loadJSON(_schema_doc_target),
+        loadJSON(_schema_doc_func),
+        loadJSON(_schema_doc_func_cache),
+        loadJSON(_schema_doc_assembly),
+        loadJSON(_schema_doc_assembly_step_call),
+        loadJSON(_schema_doc_assembly_step_series),
+        loadJSON(_schema_doc_assembly_step_map),
+    )
 
 	schema, err := loader.Compile(loadJSON(_schema_doc_root))
 	if err != nil {
