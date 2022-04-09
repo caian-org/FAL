65ccf5230307215e112c5e099beb5cbd212da985
Caian Ertl <hi@caian.org>
2022-04-09 19:11:58 -0300

    fix: issue where the build would fail if the JSON schema files could not be found

--- fal/manifest/manifest.go
+++ fal/manifest/manifest.go
@@ -1,55 +1,11 @@
+//go:build !_beforebuild
+
 package manifest
 
 import (
-    "os"
-    "path/filepath"
-    "strings"
-
 	"fal/shared/fs"
 	"sigs.k8s.io/yaml"
 )
-
-func convertYamlFileToJson(path string, info os.FileInfo, err error) error {
-    if err != nil {
-        panic(err)
-    }
-
-    if info.IsDir() || !strings.HasSuffix(path, ".yml") {
-        return nil
-    }
-
-    schemaLevel := fs.NewLocation(filepath.Dir(path))
-    schemaFilename := info.Name()
-
-    yamlSchema, err := schemaLevel.ReadFile(schemaFilename)
-    if err != nil {
-        panic(err)
-    }
-
-    jsonSchema, err := yaml.YAMLToJSON(yamlSchema)
-    if err != nil {
-        panic(err)
-    }
-
-    safeFL := []rune(schemaFilename)
-    jsonSchemaFilename := string(safeFL[:len(safeFL) - 4]) + ".json"
-
-    _, err = schemaLevel.CreateFile(jsonSchemaFilename, jsonSchema)
-    if err != nil {
-        panic(err)
-    }
-
-    return nil
-}
-
-func CreateJsonSchemaFiles() {
-    pwd, err := os.Getwd()
-    if err != nil {
-        panic(err)
-    }
-
-    filepath.Walk(filepath.Join(pwd, "manifest", "manifest.json"), convertYamlFileToJson)
-}
 
 func LoadAndValidate(rootpath *fs.Location) (*FALManifest, error) {
 	data, err := rootpath.ReadFile(".fal.yml")
--- fal/manifest/validate.go
+++ fal/manifest/validate.go
@@ -1,3 +1,5 @@
+//go:build !_beforebuild
+
 package manifest
 
 import (
